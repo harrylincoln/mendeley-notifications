7fc55d0e372c774c83faf6d386c75ebff9921ad9
Lincoln <h.lincoln@elsevier.com>
2018-08-24 16:39:19 +0100

    SVGs

--- src/Icon.js
+++ src/Icon.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import classnames from 'classnames';
 import icons from './icons';
+import {_bind} from './utils';
 
 export default class Icon extends React.Component {
   static displayName = 'ReduxToastrIcon';
@@ -9,13 +10,43 @@ export default class Icon extends React.Component {
     size: 32
   };
 
+  constructor(props) {
+    super(props);
+
+    _bind(
+      [
+        'returnDefs'
+      ],
+      this
+    );
+  }
+
+  returnDefs(props) {
+    switch (props) {
+      case 'success':
+        return (
+          <defs>
+             <circle id="path-1" cx="9" cy="9" r="9"></circle>
+             <polygon id="path-3" points="3.92693696 5.84679653 7.60389222 2.16984127 8.73526307 3.30121212 3.92693696 8.10953823 0.831388033 5.0139893 1.96275888 3.88261845"></polygon>
+         </defs>
+        );
+      case 'error':
+        return (
+          <defs>
+              <path d="M9.9068995,16.9392145 C9.80877447,17.1354646 9.64964429,17.2945948 9.45339422,17.3927198 C8.95246611,17.6431838 8.34334241,17.4401426 8.09287836,16.9392145 L0.607058076,1.96757393 C0.536653604,1.82676499 0.5,1.67149783 0.5,1.51406865 C0.5,0.954013998 0.954013998,0.5 1.51406865,0.5 L16.4857092,0.5 C16.6431384,0.5 16.7984056,0.536653604 16.9392145,0.607058076 C17.4401426,0.857522129 17.6431838,1.46664583 17.3927198,1.96757393 L9.9068995,16.9392145 Z" id="path-1"></path>
+          </defs>
+        );
+      default:
+        return;
+    }
+  }
+
   render() {
     const {size} = this.props;
     const styles = {
       width: size,
       height: size
     };
-
     return (
       <svg
         className={classnames(this.props.className, 'toastr-icon')}
@@ -24,6 +55,7 @@ export default class Icon extends React.Component {
         viewBox={`0 0 ${this.props.size} ${this.props.size}`}
         style={styles}
       >
+        {this.returnDefs(this.props.name)}
         {icons(this.props.name)}
       </svg>
     );
--- src/icons.js
+++ src/icons.js
@@ -3,7 +3,31 @@ import React from 'react';
 export default function(name) {
   switch (name) {
     case 'success':
-      return <g><path d="M27 4l-15 15-7-7-5 5 12 12 20-20z"></path></g>;
+      return (
+    <g id="Notifications" stroke="none" strokeWidth="1" fill="" fillRule="evenodd">
+            <g id="notification/positive" transform="translate(-17.000000, -13.000000)">
+                <g id="icon/action/tick/solid20" transform="translate(16.000000, 12.000000)">
+                    <g id="icon/solidcontainer" transform="translate(1.000000, 1.000000)">
+                        <mask id="mask-2" fill="white">
+                            <use xlinkHref="#path-1"></use>
+                        </mask>
+                        <use id="Mask" fill="#505050" fillRule="evenodd" xlinkHref="#path-1"></use>
+                        <g id="colour/Aqua/aqua-10" mask="url(#mask-2)" fill="#3DBA9F" fillRule="evenodd">
+                            <rect id="Rectangle-10" x="0" y="0" width="18" height="18"></rect>
+                        </g>
+                    </g>
+                    <g id="icon/10px/tick" transform="translate(5.000000, 5.000000)">
+                        <mask id="mask-4" fill="white">
+                            <use xlinkHref="#path-3"></use>
+                        </mask>
+                        <use id="tick10" fill="#000000" fillRule="nonzero" xlinkHref="#path-3"></use>
+                        <g id="colour/neutral/white" mask="url(#mask-4)" fill="#FFFFFF" fillRule="evenodd">
+                            <rect id="Rectangle-10" x="0" y="0" width="10" height="10"></rect>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </g>);
     case 'info':
       return <g>
           <path d="M14 9.5c0-0.825 0.675-1.5 1.5-1.5h1c0.825 0 1.5 0.675 1.5 1.5v1c0 0.825-0.675 1.5-1.5 1.5h-1c-0.825 0-1.5-0.675-1.5-1.5v-1z"></path>
@@ -17,7 +41,30 @@ export default function(name) {
           <path d="M16 22c-1.105 0-2-0.895-2-2v-6c0-1.105 0.895-2 2-2s2 0.895 2 2v6c0 1.105-0.895 2-2 2z"></path>
         </g>;
     case 'error':
-      return <g><path d="M12,0C5.373,0,0,5.373,0,12s5.373,12,12,12s12-5.373,12-12S18.627,0,12,0z M12,19.66 c-0.938,0-1.58-0.723-1.58-1.66c0-0.964,0.669-1.66,1.58-1.66c0.963,0,1.58,0.696,1.58,1.66C13.58,18.938,12.963,19.66,12,19.66z M12.622,13.321c-0.239,0.815-0.992,0.829-1.243,0c-0.289-0.956-1.316-4.585-1.316-6.942c0-3.11,3.891-3.125,3.891,0 C13.953,8.75,12.871,12.473,12.622,13.321z"/></g>;
+      return (
+        <g id="Notifications" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
+        <g id="notification/error" transform="translate(-17.000000, -13.000000)">
+            <g id="icon/warning-contained" transform="translate(16.000000, 12.000000)">
+                <g id="icon/action/tick/solid20" transform="translate(10.000000, 10.000000) scale(1, -1) translate(-10.000000, -10.000000) ">
+                    <rect id="Rectangle" x="0" y="0" width="20" height="20"></rect>
+                    <g id="container" transform="translate(1.000000, 1.000000)">
+                        <mask id="mask-2" fill="white">
+                            <use xlinkHref="#path-1"></use>
+                        </mask>
+                        <use id="Mask" fill="#D01928" xlinkHref="#path-1"></use>
+                        <g id="colour/MLDYRed/Red-10" mask="url(#mask-2)" fill="#D01928">
+                            <g transform="translate(-1.000000, -1.000000)" id="Rectangle-10">
+                                <rect x="0" y="0" width="20" height="20"></rect>
+                            </g>
+                        </g>
+                    </g>
+                    <path d="M10,7.5 L10,13" id="Line" stroke="#FFFFFF" strokeWidth="1.8"></path>
+                    <path d="M10,4 L10,6" id="Line" stroke="#FFFFFF" strokeWidth="1.8"></path>
+                </g>
+            </g>
+        </g>
+    </g>
+      );
     default:
       return null;
   }
